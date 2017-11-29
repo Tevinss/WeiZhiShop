dfaf1fee95bbd9e7b02dc16ad710823c99f1bd23
tevins <1913739434@qq.com>
2017-11-29 09:22:32 +0800

    fitst commit

---
+++ app/src/main/java/tevins/com/weizhishop/fragment/HomeFrament.java
@@ -0,0 +1,21 @@
+package tevins.com.weizhishop.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yewyw on 2017/11/29/0029.
+ */
+
+public class HomeFrament extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
